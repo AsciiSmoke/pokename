1ec94bbbf073d3d7efbf03e6f4b23df5f1ebe584
Craig <AsciiSmoke@Gmail.com>
2015-06-22 21:21:33 +0100

    Added storage of previously displayed Pokemon and filter function to get a unique one on each call

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -9,7 +9,9 @@
     $.extend(PokeApi, {
 
         apiUrl: "http://pokeapi.co/api/v1/pokedex/1/",
+        loaded: false,
         pokeData: [],
+        displayedPokemon: [],
 
         // Init method to set up data
         init: function () {
@@ -23,35 +25,45 @@
             console.log("Fetching data...");
             $.get(PokeApi.apiUrl, function (data) {
 
-
+                // Check we actually got something usable
                 console.log("Verifying data...");
-                // Check you actually got something usable
                 if (data.pokemon == undefined || data.pokemon.length <= 0) {
                     // Call fail method to alert user
                     PokeApi.ajaxFailed();
                 }
 
-
-                console.log("Sorting and storing data...");
                 // Sort and store the data in an object level variable for safe-keeping
+                console.log("Sorting and storing data...");
                 PokeApi.pokeData = PokeApi.sortData(data.pokemon);
                 console.log(PokeApi.pokeData);
 
-                var filteredData = PokeApi.filterData("c");
-
-                console.log(filteredData);
             })
                 .fail(function () {
                     // Call fail method to alert user
                     PokeApi.ajaxFailed();
                 })
                 .done(function () {
+                    // Set loaded flag to allow the application can interact with the data
+                    if (PokeApi.pokeData != undefined) {
+                        PokeApi.loaded = true;
+
+                        // Test getNewPokemon
+                        for (var ii = 0; ii <= 100; ii++) {
+                            var newPokemon = PokeApi.getNewPokemon("c");
+                            if (newPokemon) {
+                                console.log(PokeApi.getNewPokemon("c"));
+                            }else{
+                                break;
+                            }
+                        }
+                    }
                 })
                 .always(function () {
                 });
         },
 
         ajaxFailed: function () {
+            PokeApi.loaded = false;
             alert("Sorry, I couldn't get the Pokemon data.\r\rPlease try again.");
         },
 
@@ -67,11 +79,32 @@
             });
         },
 
-        filterData: function (char) {
-            console.log("Finding Pokemon that start with '"+char+"' ...");
-            return PokeApi.pokeData.filter(function (poke) {
-                return poke.name.substr(0, 1).toLowerCase() == char;
+        getNewPokemon: function (char) {
+            console.log("Finding Random Pokemon that start with '" + char + "' ...");
+
+            // Create a shortlist by filtering down to the first character and ignoring any previously returned pokemon
+            var filteredShortList = PokeApi.pokeData.filter(function (poke) {
+                return (
+                    poke.name.substr(0, 1).toLowerCase() == char
+                    &&
+                    PokeApi.displayedPokemon.indexOf(poke) == -1
+                )
             });
+
+            var shortlistCount = (filteredShortList.length);
+
+            // Pick a PokeMon from the filtered short list
+            if (shortlistCount > 0) {
+                var randomSelector = Math.round(Math.random() * (shortlistCount - 1));
+                var pokeToReturn = filteredShortList[randomSelector];
+
+                // Push the selected pokemon into a list of returned pokemon and return
+                PokeApi.displayedPokemon.push(pokeToReturn);
+                return pokeToReturn;
+            } else {
+                alert("No new Pokemon found starting with '" + char + "'");
+                return null;
+            }
         }
 
     });
